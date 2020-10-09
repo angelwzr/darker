28beb27acbd73964a19173b04845d33596481520
angelwzr <angel@windowz.ru>
2020-10-09 22:08:00 +0300

    Working time picker

--- darker.app/Views/SettingsSchedulerDialog.xaml.cs
+++ darker.app/Views/SettingsSchedulerDialog.xaml.cs
@@ -1,4 +1,6 @@
 using ModernWpf.Controls;
+using System;
+using System.Windows;
 
 namespace darker.Viewes
 {
@@ -7,6 +9,19 @@ namespace darker.Viewes
         public SettingsSchedulerDialog()
         {
             InitializeComponent();
+
+            //TimePickerDark.Culture = CultureInfo.CurrentUICulture.Name.ToString;
+            //TimePickerLight.Culture = CultureInfo.CurrentUICulture.Name.ToString;
+        }
+
+        private void TimePickerLight_SelectedDateTimeChanged(object sender, RoutedPropertyChangedEventArgs<DateTime?> e)
+        {
+            TestControl.Content = e.NewValue?.ToShortTimeString() ?? "null";
+        }
+
+        private void TimePickerDark_SelectedDateTimeChanged(object sender, RoutedPropertyChangedEventArgs<DateTime?> e)
+        {
+            TestControl.Content = e.NewValue?.ToShortTimeString() ?? "null";
         }
     }
 }
